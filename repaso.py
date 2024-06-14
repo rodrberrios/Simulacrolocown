33d2451970c852480fab3668201863801f00a989
rodrberrios <rodr.berrios@duocuc.cl>
2024-06-14 11:05:17 -0400

    ejercicio solo con menu

---
+++ repaso.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+os.system("cls")
+
+
+
+
+
+while True:
+    print("Menu Trabajadores")
+    print("")
+    print("""
+           1. Registrar trabajador
+           2. Listar Trabajadores
+           3. Imprimir plantilla de sueldos
+           4. salir
+          """)
+    opc=int(input("Ingrese Opcion\n> "))
+
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    elif opc==4:
+        break
