6c3a7954979eb1f7b9d6d08f8aaeafa98b5b65f0
rodrberrios <rodr.berrios@duocuc.cl>
2024-06-14 11:24:19 -0400

    opcion 1 lista sin validaciones

--- repaso.py
+++ repaso.py
@@ -4,7 +4,7 @@ import time
 os.system("cls")
 
 
-
+lista_trabajadores=[]
 
 
 while True:
@@ -19,6 +19,21 @@ while True:
     opc=int(input("Ingrese Opcion\n> "))
 
     if opc==1:
+        print("registrar")
+        nombre_apellido=input("Ingrese nombre y apeññido\n > ")
+        cargo=int(input("Ingrese cargo\n 1. CEO\n 2. Desarrollador\n 3. Analista\n > "))
+        bruto=int(input("Ingrese sueldo bruto\n > "))
+        
+        salud=bruto*0.07
+        afp=bruto*0.12
+        total=afp+salud
+        liquido=bruto-total
+
+
+        trabajador=[nombre_apellido,cargo,bruto,salud,afp,liquido]
+        lista_trabajadores.append(trabajador)
+
+        print("Trabajador registrado con éxito")
         pass
     elif opc==2:
         pass
